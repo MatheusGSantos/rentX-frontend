46b61a472aa315c47ade04aefbd3a79c22800e3a
Matheus Gama <mths.gm@gmail.com>
2023-08-10 23:46:52 -0300

    config

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,33 +1,30 @@
 import { Route, Routes } from 'react-router-dom';
+import RedirectHandler from './RedirectHandler';
+import { NotFound } from '../pages/NotFound';
 // import { Home } from '../pages/Home';
-// import { NotFound } from '../pages/NotFound';
-import { Wrapper } from '../pages/Wrapper';
 // import LogIn from '../pages/LogIn';
-// import RedirectHandler from './RedirectHandler';
 
 export function RoutesIndexer() {
   return (
     <Routes>
-      <Route path='/' element={<Wrapper />}>
-        {/* <Route index element={<Home />} /> */}
-        {/* <Route
-          path='login'
-          element={
-            <RedirectHandler handlerType='logged'>
-              <LogIn />
-            </RedirectHandler>
-          }
-        />
-        <Route
-          path='signup'
-          element={
-            <RedirectHandler handlerType='logged'>
-              <SignUp />
-            </RedirectHandler>
-          }
-        /> */}
-        {/* <Route path='*' element={<NotFound />} /> */}
-      </Route>
+      {/* <Route index element={<Home />} />
+      <Route
+        path='/login'
+        element={
+          <RedirectHandler handlerType='logged'>
+            <LogIn />
+          </RedirectHandler>
+        }
+      />
+      <Route
+        path='/signup'
+        element={
+          <RedirectHandler handlerType='logged'>
+            <SignUp />
+          </RedirectHandler>
+        }
+      /> */}
+      <Route path='*' element={<NotFound />} />
     </Routes>
   );
 }
